b0f1fddca908b246b23903b22ae8aa259e9b2b76
Spratch <contact@josephclenet.fr>
2026-05-21 18:02:31 +0200

    build(sanity): init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+import sanity from "@sanity/astro";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    sanity({
+      projectId: "a7gwf3qs",
+      dataset: "production",
+      useCdn: false,
+      apiVersion: "2026-05-26",
+      studioBasePath: "/admin",
+    }),
+    react(),
+  ],
+});
