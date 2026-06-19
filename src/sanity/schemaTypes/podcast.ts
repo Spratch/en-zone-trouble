5be9d60eb40f6a0047e396950b9ba547aad44de4
Spratch <contact@josephclenet.fr>
2026-06-19 12:19:49 +0200

    fix(schemas): cover image validation rule

--- src/sanity/schemaTypes/podcast.ts
+++ src/sanity/schemaTypes/podcast.ts
@@ -12,7 +12,7 @@ export const podcastSchema = defineType({
       title: "Titre",
       description: "Titre du podcast",
       type: "string",
-      validation: (Rule) => Rule.required(),
+      validation: (Rule) => Rule.required()
     }),
     defineField({
       name: "slug",
@@ -21,9 +21,9 @@ export const podcastSchema = defineType({
         "Identifiant unique du podcast, cliquer sur Générer après avoir renseigné le titre",
       type: "slug",
       options: {
-        source: "title",
+        source: "title"
       },
-      validation: (Rule) => Rule.required(),
+      validation: (Rule) => Rule.required()
     }),
     defineField({
       name: "date",
@@ -31,36 +31,40 @@ export const podcastSchema = defineType({
       description: "Date de création du podcast",
       type: "date",
       options: { dateFormat: "DD/MM/YYYY" },
-      validation: (Rule) => Rule.required(),
+      validation: (Rule) => Rule.required()
     }),
     defineField({
       name: "cover",
       title: "Couverture",
       description: "Image de couverture du podcast",
       type: "image",
+      validation: (rule) =>
+        rule.required().custom((value) => {
+          return value?.asset ? true : "Une image doit être sélectionnée";
+        }),
       fields: [
         defineField({
           name: "orientation",
           title: "Orientation",
           description: "Sélectionner l'orientation de l'image",
-          type :"string",
+          type: "string",
           initialValue: "landscape",
+          validation: (Rule) => Rule.required(),
           options: {
             list: [
-              { title:"Paysage", value:"landscape" },
-              { title:"Portrait", value:"portrait" }
+              { title: "Paysage", value: "landscape" },
+              { title: "Portrait", value: "portrait" }
             ]
           }
         })
-      ],
-      validation: (Rule) => Rule.required(),
+      ]
     }),
     defineField({
       name: "synopsis",
       title: "Synopsis",
       description: "Synopsis du podcast",
       type: "customBlock",
-      validation: (Rule) => Rule.required(),
+      validation: (Rule) => Rule.required()
     }),
     defineField({
       name: "episodes",
@@ -80,7 +84,7 @@ export const podcastSchema = defineType({
               title: "Titre",
               description: "Titre de l'épisode",
               type: "string",
-              validation: (Rule) => Rule.required(),
+              validation: (Rule) => Rule.required()
             }),
             defineField({
               name: "mp3",
@@ -89,67 +93,67 @@ export const podcastSchema = defineType({
               description: "Audio de l'épisode",
               type: "mux.video",
               options: {
-                acceptedMimeTypes: ["audio/*"],
-              },
-            }),
-          ],
-        },
-      ],
+                acceptedMimeTypes: ["audio/*"]
+              }
+            })
+          ]
+        }
+      ]
     }),
     defineField({
       name: "infos",
       title: "Infos",
       description: "Entrer les infos du spectacle (dates, lieu, etc.)",
-      type: "customBlock",
+      type: "customBlock"
     }),
     defineField({
       name: "supports",
       title: "Soutiens",
       description: "Soutenu par…",
       type: "text",
-      rows: 2,
+      rows: 2
     }),
     defineField({
       name: "production",
       title: "Production",
       description: "Crédits de production",
       type: "text",
-      rows: 2,
+      rows: 2
     }),
     defineField({
       name: "credits",
       title: "Crédits",
-      type: "credits",
+      type: "credits"
     }),
     defineField({
       name: "gallery",
       title: "Galerie",
       description: "Images liées au podcast",
-      type: "gallery",
+      type: "gallery"
     }),
     defineField({
       name: "links",
       title: "Liens",
       description: "Liste des liens liés au podcast",
-      type: "linksArray",
+      type: "linksArray"
     }),
     defineField({
       name: "press",
       title: "Presse et récompenses",
       description: "Liste des liens de presse liés au podcast",
-      type: "linksArray",
-    }),
+      type: "linksArray"
+    })
   ],
   preview: {
     select: {
       title: "title",
       subtitle: "date",
-      media: "cover",
+      media: "cover"
     },
     prepare: ({ title, subtitle, media }) => ({
       title,
       subtitle: subtitle.split("-")[0],
-      media,
-    }),
-  },
+      media
+    })
+  }
 });
--- src/sanity/schemaTypes/show.ts
+++ src/sanity/schemaTypes/show.ts
@@ -12,7 +12,7 @@ export const showSchema = defineType({
       title: "Titre",
       description: "Titre du spectacle",
       type: "string",
-      validation: (Rule) => Rule.required(),
+      validation: (Rule) => Rule.required()
     }),
     defineField({
       name: "slug",
@@ -21,9 +21,9 @@ export const showSchema = defineType({
         "Identifiant unique du spectacle, cliquer sur Générer après avoir renseigné le titre",
       type: "slug",
       options: {
-        source: "title",
+        source: "title"
       },
-      validation: (Rule) => Rule.required(),
+      validation: (Rule) => Rule.required()
     }),
     defineField({
       name: "date",
@@ -31,101 +31,103 @@ export const showSchema = defineType({
       description: "Date de création du spectacle",
       type: "date",
       options: { dateFormat: "DD/MM/YYYY" },
-      validation: (Rule) => Rule.required(),
+      validation: (Rule) => Rule.required()
     }),
     defineField({
       name: "cover",
       title: "Couverture",
       description: "Image de couverture du spectacle",
       type: "image",
-      options: {
-        hotspot: true,
-      },
+      validation: (rule) =>
+        rule.required().custom((value) => {
+          return value?.asset ? true : "Une image doit être sélectionnée";
+        }),
       fields: [
         defineField({
           name: "orientation",
           title: "Orientation",
           description: "Sélectionner l'orientation de l'image",
-          type :"string",
+          type: "string",
           initialValue: "landscape",
+          validation: (Rule) => Rule.required(),
           options: {
             list: [
-              { title:"Paysage", value:"landscape" },
-              { title:"Portrait", value:"portrait" }
+              { title: "Paysage", value: "landscape" },
+              { title: "Portrait", value: "portrait" }
             ]
           }
         })
-      ],
-      validation: (Rule) => Rule.required(),
+      ]
     }),
     defineField({
       name: "synopsis",
       title: "Synopsis",
       description: "Synopsis du spectacle",
       type: "customBlock",
-      validation: (Rule) => Rule.required(),
+      validation: (Rule) => Rule.required()
     }),
     defineField({
       name: "excerpt",
       title: "Extrait",
-      description: "Entrer le texte, il sera affiché avec une typographie scripte",
+      description:
+        "Entrer le texte, il sera affiché avec une typographie scripte",
       type: "text",
-      rows: 3,
+      rows: 3
     }),
     defineField({
       name: "infos",
       title: "Infos",
       description: "Entrer les infos du spectacle (dates, lieu, etc.)",
-      type: "customBlock",
+      type: "customBlock"
     }),
     defineField({
       name: "supports",
       title: "Soutiens",
       description: "Soutenu par…",
       type: "text",
-      rows: 2,
+      rows: 2
     }),
     defineField({
       name: "production",
       title: "Production",
       description: "Crédits de production",
       type: "text",
-      rows: 2,
+      rows: 2
     }),
     defineField({
       name: "credits",
       title: "Crédits",
-      type: "credits",
+      type: "credits"
     }),
     defineField({
       name: "gallery",
       title: "Galerie",
       description: "Images liées au spectacle",
-      type: "gallery",
+      type: "gallery"
     }),
     defineField({
       name: "links",
       title: "Liens",
       description: "Liste des liens liés au spectacle",
-      type: "linksArray",
+      type: "linksArray"
     }),
     defineField({
       name: "press",
       title: "Presse et récompenses",
       description: "Liste des liens de presse liés au spectacle",
-      type: "linksArray",
-    }),
+      type: "linksArray"
+    })
   ],
   preview: {
     select: {
       title: "title",
       subtitle: "date",
-      media: "cover",
+      media: "cover"
     },
     prepare: ({ title, subtitle, media }) => ({
       title,
       subtitle: subtitle.split("-")[0],
-      media,
-    }),
-  },
+      media
+    })
+  }
 });
